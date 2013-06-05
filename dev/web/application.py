fa02f9d4af9c2816d54b37ea191c5b0d9305598e
gromero <german.d.romero@gmail.com>
2013-06-05 16:28:42 -0300

    #20 Creacion app estacionamientos - template

--- dev/web/application.py
+++ dev/web/application.py
@@ -1,14 +1,20 @@
 from application import app, db
 from flask import url_for, redirect, render_template, request
 from application.modules.login import login_blueprint
+from flask.ext.login import login_required
 
 app.register_blueprint(login_blueprint)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/app")
+@login_required
+def indexapp():
+    return render_template('index-app.html')
 	
-if __name__ == '__main__':
+if __name__ == '__main__':        
     db.create_all()
     app.debug=True
     app.run('127.0.0.1', 8000)
