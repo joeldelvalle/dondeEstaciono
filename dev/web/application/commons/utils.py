2cc9a60a9d0050eed96536362df24382f8af261a
joeldelvalle <joelmarcosdelvalle@gmail.com>
2013-07-04 17:35:42 -0300

    #17 ABM tipo de vehiculo

--- dev/web/application/commons/utils.py
+++ dev/web/application/commons/utils.py
@@ -2,3 +2,8 @@ __author__ = 'gromero'
 
 def strip(value):
     return value.replace(" ", "")
+
+def cleanFields(response, form):
+    if response == 'success':
+        for field in form:
+            field.data = None
