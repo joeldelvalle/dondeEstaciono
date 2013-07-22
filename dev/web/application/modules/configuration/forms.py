f09fb8b816886734747633df4010c6533d883a59
joeldelvalle <joelmarcosdelvalle@gmail.com>
2013-07-22 12:32:12 -0300

    #27 pantalla abm tipo de frecuencia

--- dev/web/application/modules/configuration/forms.py
+++ dev/web/application/modules/configuration/forms.py
@@ -1,6 +1,9 @@
 from application import db
-from application.models.model import *
 from flask.ext import wtf
+from application.commons import databaseCache
+import logging
+#from flask.ext.wtf import Form, TextField, BooleanField, FileField, file_required,         RadioField
+#from flask.ext.wtf import Required
 
 # clase que representa el formulario en el abm de vehicleType
 class VehicleTypeForm(wtf.Form):
@@ -16,7 +19,21 @@ class VehicleTypeForm(wtf.Form):
 class FrequencyTypeForm(wtf.Form):
     
     description = wtf.TextField(validators=[wtf.required()])
+    
+    type = wtf.RadioField(choices=[(1,'Frecuencia'),(2,'Fijo')])
+
+    time = wtf.IntegerField(validators=[wtf.required()])
+
+    timeType = wtf.SelectField(u'TimeType')
+        
+    priority = wtf.IntegerField(validators=[wtf.required()])            
+       
+    combinablePreviousFrequency = wtf.RadioField(choices=[(0,'SI'),(1,'No')])    
+    
 
     def __init__(self, *args, **kwargs):
         kwargs['csrf_enabled'] = False
-        super(FrequencyTypeForm, self).__init__(*args, **kwargs)        
+        super(FrequencyTypeForm, self).__init__(*args, **kwargs)
+        
+        self.timeType.choices = [(g.id, g.description) for g in databaseCache.getTimeTypeList()]
+        
