dc748e269a32684ec5f9979f9bc8fa1f8571a04b
joeldelvalle <joelmarcosdelvalle@gmail.com>
2013-07-25 13:18:22 -0300

    #27 pantalla abm tipo de frecuencia

--- dev/web/application/commons/utils.py
+++ dev/web/application/commons/utils.py
@@ -7,3 +7,10 @@ def cleanFields(response, form):
     if response == 'success':
         for field in form:
             field.data = None
+
+
+# borra los espacios en blanco de un objeto json
+def cleanWhiteSpaceFromJson(json):
+    newJson = json.replace('": ', '":')
+    newJson = newJson.replace(', "', ',"')
+    return newJson
